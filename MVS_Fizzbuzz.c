6c10f04065ed23a94883def623c5e3dab3a22ccc
jeanj <Jeanjnr>
2022-05-13 09:25:56 +0200

    add fichier MVS_Fizzbuzz.c MVS_FizzBuzz2.h

--- MVS_Fizzbuzz.c
+++ MVS_Fizzbuzz.c
@@ -6,14 +6,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#define FIZZ "Fizz"
-#define BUZZ "Buzz"
-int bb (void) {
+#include "MVS_FizzBuzz.h"
+int main (void) {
     for (int i = 1; i <= 100; i++) {
         if (i % 15 == 0) {
             printf ("%s%s\n", FIZZ, BUZZ);
         } else if (i % 3 == 0) {
-
             printf ("%s\n", FIZZ);
         } else if (i % 5 == 0) {
             printf ("%s\n", BUZZ);
@@ -21,5 +19,4 @@ int bb (void) {
             printf ("%d\n", i);
         }
     }
-    return EXIT_SUCCESS;
-}
+    return EXIT_SUCCESS
